3e9ed65e9368ecc7b8a59f88b7c05cf9af90b57c
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2025-10-16 10:22:11 +0900

    update ex-08_1.sql

--- from-teacher/03/drill/ex-08_1.sql
+++ from-teacher/03/drill/ex-08_1.sql
@@ -2,6 +2,6 @@ SELECT
   id,
   name,
   created_on AS "created_on_1",
-  '令和' || DATE_PART('year', created_on) - 2018 || '年' || TO_CHAR(created_on, 'MM月DD日') AS "created_on_2"
+  '令和' || DATE_PART('year', created_on) - 2018 || '年' || TO_CHAR(created_on, 'MM"月"DD"日"') AS "created_on_2"
 FROM
   s_characters;
