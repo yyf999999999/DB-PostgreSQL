675c488d0eb645321cf726ab1d7b7304291377ab
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2025-10-23 11:31:11 +0900

    update tmp.sql

--- sql/04/tmp.sql
+++ sql/04/tmp.sql
@@ -1,6 +1,8 @@
 SELECT
-    *
+    id,
+    guild AS "所属ギルド",
+    name AS "名前",
+    level AS "レベル",
+    TO_CHAR(last_login_at, 'YYYY"/"MM"/"DD HH24":"MI') AS "最終ログイン"
 FROM
-    s_users;
-
-a
+    s_characters;
